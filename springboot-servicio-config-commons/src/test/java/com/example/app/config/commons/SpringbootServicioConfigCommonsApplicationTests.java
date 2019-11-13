4da378ab2eeebd8903585759c37d9c87e39ed383
mefarina <melissa.a.farina@gmail.com>
2019-11-13 11:15:50 -0300

    Commons

---
+++ springboot-servicio-config-commons/src/test/java/com/example/app/config/commons/SpringbootServicioConfigCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.config.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- springboot-servicio-productos/src/main/java/com/example/demo/models/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.entity.Producto;
-
-public interface ProductoDao extends CrudRepository<Producto, Long> {
-
-}
